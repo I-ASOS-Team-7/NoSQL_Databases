08160b2390cec7bff6b50c632f6e738e2572871d
Raychani1 <rajcsanyi.ladislav.it@gmail.com>
2022-10-17 17:30:31 +0200

    Refactor Statistics

--- src/stats/statistics.py
+++ src/stats/statistics.py
@@ -6,161 +6,73 @@ import pandas as pd
 
 class Statistics:
 
-    """Represents the Database Testing Statistics."""
+	"""Represents the Database Testing Statistics."""
 
-    def __init__(
-        self,
-        export_folder_path: Optional[str] = os.path.join(
-            os.getcwd(), 'data', 'output'
-        )
-    ) -> None:
-        """Initializes the Statistics Class.
+	def __init__(
+		self,
+		export_folder_path: Optional[str] = os.path.join(
+			os.getcwd(), 'data', 'output'
+		)
+	) -> None:
+		"""Initializes the Statistics Class.
 
-        Args:
-            export_folder_path (Optional[str]): Database Testing Statistics 
-            result export folder path. Defaults to os.path.join(os.getcwd(), 
-            'data', 'output').
-        """
-        self.__export_folder_path: str = export_folder_path
-        self.__insert_data: pd.DataFrame = None
-        self.__update_data: pd.DataFrame = None
-        self.__delete_data: pd.DataFrame = None
+		Args:
+			export_folder_path (Optional[str]): Database Testing Statistics 
+			result export folder path. Defaults to os.path.join(os.getcwd(), 
+			'data', 'output').
+		"""
+		self.__export_folder_path: str = export_folder_path
+		self.__execution_times: pd.DataFrame = None
 
-    def add_insert_time(
-        self,
-        database_type: str,
-        database: str,
-        dataset: str,
-        time: float
-    ) -> None:
-        """Adds new Insert Time value to DataFrame.
+	def add_execution_time(
+		self,
+		database_type: str,
+		database: str,
+		dataset: str,
+		action: str,
+		time: float
+	) -> None:
+		"""Adds new Insert Time value to DataFrame.
 
-        Args:
-            database_type (str): Type of NoSQL Database.
-            database (str): Name of the NoSQL Database.
-            dataset (str): Name of the Dataset/Collection in NoSQL Database.
-            time (float): Execution Time.
-        """
-        data = {
-            'database_type': database_type,
-            'database': database,
-            'dataset': dataset,
-            'time': time
-        }
+		Args:
+			database_type (str): Type of NoSQL Database.
+			database (str): Name of the NoSQL Database.
+			dataset (str): Name of the Dataset/Collection in NoSQL Database.
+			time (float): Execution Time.
+		"""
+		data = {
+			'database_type': database_type,
+			'database': database,
+			'dataset': dataset,
+			'action': action,
+			'time': time
+		}
 
-        if self.__insert_data is None:
-            self.__insert_data = pd.DataFrame(
-                data,
-                index=[0]
-            )
-        else:
-            self.__insert_data = pd.concat(
-                [
-                    self.__insert_data,
-                    pd.DataFrame(
-                        data,
-                        index=[0]
-                    )
-                ],
-                axis=0,
-                ignore_index=True
-            )
+		if self.__execution_times is None:
+			self.__execution_times = pd.DataFrame(
+				data,
+				index=[0]
+			)
+		else:
+			self.__execution_times = pd.concat(
+				[
+					self.__execution_times,
+					pd.DataFrame(
+						data,
+						index=[0]
+					)
+				],
+				axis=0,
+				ignore_index=True
+			)
 
-    def add_update_time(
-        self,
-        database_type: str,
-        database: str,
-        dataset: str,
-        time: float
-    ) -> None:
-        """Adds new Update Time value to DataFrame.
-
-        Args:
-            database_type (str): Type of NoSQL Database.
-            database (str): Name of the NoSQL Database.
-            dataset (str): Name of the Dataset/Collection in NoSQL Database.
-            time (float): Execution Time.
-        """
-        data = {
-            'database_type': database_type,
-            'database': database,
-            'dataset': dataset,
-            'time': time
-        }
-
-        if self.__update_data is None:
-            self.__update_data = pd.DataFrame(
-                data,
-                index=[0]
-            )
-        else:
-            self.__update_data = pd.concat(
-                [
-                    self.__update_data,
-                    pd.DataFrame(
-                        data,
-                        index=[0]
-                    )
-                ],
-                axis=0,
-                ignore_index=True
-            )
-
-    def add_delete_time(
-        self,
-        database_type: str,
-        database: str,
-        dataset: str,
-        time: float
-    ) -> None:
-        """Adds new Delete Time value to DataFrame.
-
-        Args:
-            database_type (str): Type of NoSQL Database.
-            database (str): Name of the NoSQL Database.
-            dataset (str): Name of the Dataset/Collection in NoSQL Database.
-            time (float): Execution Time.
-        """
-        data = {
-            'database_type': database_type,
-            'database': database,
-            'dataset': dataset,
-            'time': time
-        }
-
-        if self.__delete_data is None:
-            self.__delete_data = pd.DataFrame(
-                data,
-                index=[0]
-            )
-        else:
-            self.__delete_data = pd.concat(
-                [
-                    self.__delete_data,
-                    pd.DataFrame(
-                        data,
-                        index=[0]
-                    )
-                ],
-                axis=0,
-                ignore_index=True
-            )
-
-    def export_data_to_csv(self) -> None:
-        """Exports each Time DataFrame to separate CSV File."""
-        datasets = [self.__insert_data, self.__update_data, self.__delete_data]
-        filenames = [
-            'NoSQL_Databases_Insert_Times',
-            'NoSQL_Databases_Update_Times',
-            'NoSQL_Databases_Delete_Times'
-        ]
-
-        for dataset, filename in zip(datasets, filenames):
-            if dataset is not None:
-                dataset.to_csv(
-                    os.path.join(
-                        self.__export_folder_path,
-                        f'{filename}.csv'
-                    ),
-                    index=False
-                )
+	def export_data_to_csv(self) -> None:
+		"""Exports each Time DataFrame to separate CSV File."""
+		if self.__execution_times is not None:
+			self.__execution_times.to_csv(
+				os.path.join(
+					self.__export_folder_path,
+					f'NoSQL_Databases_Execution_Times.csv'
+				),
+				index=False
+			)
