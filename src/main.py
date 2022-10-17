cef79aec325e51f2405052d64d1398d21cbe6b70
Raychani1 <rajcsanyi.ladislav.it@gmail.com>
2022-10-17 17:33:29 +0200

    Refactor files

--- src/main.py
+++ src/main.py
@@ -2,14 +2,20 @@ import statistics
 from dotenv import load_dotenv
 from stats.statistics import Statistics
 
-from utils import run_mongodb
+from utils import (
+	run_couchdb,
+	run_mongodb
+)
 
 
 if __name__ == '__main__':
-    load_dotenv()
+	load_dotenv()
 
-    statistics = Statistics()
+	statistics = Statistics()
 
-    run_mongodb(statistics)
+	run_mongodb(statistics)
+	# run_couchdb(statistics)
 
-    
+	statistics.export_data_to_csv()
+
+	
