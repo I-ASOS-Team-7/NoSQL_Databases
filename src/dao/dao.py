7fc91070a82a0b8e403890761d0dfc363d8dbc1b
Raychani1 <rajcsanyi.ladislav.it@gmail.com>
2022-10-18 22:28:18 +0200

    Add new abstract method

--- src/dao/dao.py
+++ src/dao/dao.py
@@ -3,26 +3,30 @@ from abc import ABC, abstractmethod
 
 class DAO(ABC):
 
-	@abstractmethod
-	def create_connection(self, **kwargs):
-		pass
+    @abstractmethod
+    def create_connection(self, **kwargs) -> None:
+        pass
 
-	@abstractmethod
-	def read_data(self, **kwargs):
-		pass
+    @abstractmethod
+    def read_data(self, **kwargs) -> None:
+        pass
 
-	@abstractmethod
-	def insert_data(self, **kwargs):
-		pass
+    @abstractmethod
+    def insert_data(self, **kwargs) -> None:
+        pass
 
-	@abstractmethod
-	def update_data(self, **kwargs):
-		pass
+    @abstractmethod
+    def update_data(self, **kwargs) -> None:
+        pass
 
-	@abstractmethod
-	def delete_data(self, **kwargs):
-		pass
+    @abstractmethod
+    def delete_data(self, **kwargs) -> None:
+        pass
 
-	@abstractmethod
-	def close_connection(self):
-		pass
+    @abstractmethod
+    def close_connection(self) -> None:
+        pass
+
+    @abstractmethod
+    def populate_database(self, data_folder: str):
+        pass
