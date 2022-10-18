e5935a6e16d59e1dcc1cc49fe778fdd3051c2468
Raychani1 <rajcsanyi.ladislav.it@gmail.com>
2022-10-18 22:25:52 +0200

    Refactor file

--- src/stats/statistics.py
+++ src/stats/statistics.py
@@ -2,7 +2,6 @@ import os
 from typing import Optional
 
 import pandas as pd
-import plotly.graph_objects as go
 import plotly.express as px
 
 
@@ -74,7 +73,12 @@ class Statistics:
 				ignore_index=True
 			)
 
-	def __display_statistics(self, database_type: str, dataset: str, action: str):
+	def __display_statistics(
+		self, 
+		database_type: str, 
+		dataset: str, 
+		action: str
+	) -> None:
 		print(
 			self.__execution_times[
 				(self.__execution_times['database_type'] == database_type) &
@@ -86,9 +90,15 @@ class Statistics:
 
 	def display_statistics(self) -> None:
 		"""Displays basic statistics and distribution of execution times."""
-		for database_type in ['CouchDB', 'MongoDB', 'Redis']:
-			for dataset in sorted(os.listdir(os.path.join(os.getcwd(), 'data', 'json_data'))):
-				for action in ['insert', 'read', 'update', 'delete']:
+		for database_type in sorted(
+			list(self.__execution_times['database_type'].unique())
+		):
+			for dataset in sorted(
+				list(self.__execution_times['dataset'].unique())
+			):
+				for action in sorted(
+					list(self.__execution_times['action'].unique())
+				):
 					print(f"{database_type} {dataset.split('.')[0]} {action}")
 					self.__display_statistics(
 						database_type=database_type,
@@ -113,7 +123,10 @@ class Statistics:
 		)
 
 		fig.update_layout(
-			title=f'NoSQL Databases - {action.capitalize()} ({self.__iterations} Iterations)',
+			title=(
+				f'NoSQL Databases - {action.capitalize()}' 
+				f'({self.__iterations} Iterations)'
+			),
 			xaxis_title='Avg. Time (sec)',
 			yaxis_title='Dataset',
 			legend_title='Database Type',
@@ -124,23 +137,24 @@ class Statistics:
 		)
 
 		filename = f'NoSQL_Databases_-_{action.capitalize()}'
-		html_export_path = os.path.join(self.__export_folder_path, 'plots', 'html')
-		png_export_path = os.path.join(self.__export_folder_path, 'plots', 'png')
+		html_export_path = os.path.join(
+			self.__export_folder_path, 'plots', 'html'
+		)
 
 		if not os.path.exists(html_export_path):
 			os.mkdir(html_export_path)
 
-		if not os.path.exists(png_export_path):
-			os.mkdir(png_export_path)
-
-		fig.write_html(os.path.join(html_export_path,f'{filename}_{self.__iterations}.html'))
-		fig.write_image(os.path.join(png_export_path,f'{filename}_{self.__iterations}.png'))
+		fig.write_html(
+			os.path.join(
+				html_export_path,f'{filename}_{self.__iterations}.html'
+			)
+		)
 
 		fig.show()
 
 	def export_plots(self) -> None:
 		"""Exports every Execution Time Plot for each action."""
-		for action in ['insert', 'read', 'update', 'delete']:
+		for action in sorted(list(self.__execution_times['action'].unique())):
 			self.__export_plot(action=action)
 
 	def export_data_to_csv(self) -> None:
